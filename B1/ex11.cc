e6748b845ad36f9fef7dce45011ada7bd50dae00
LowTechTurtle <dongquocthang1235@gmail.com>
2023-11-24 09:13:43 +0700

    update

--- B1/ex11.cc
+++ B1/ex11.cc
@@ -1,21 +1,41 @@
 #include<iostream>
-
 using namespace std;
 
-int main() 
-{
-	long long int n, m;
-	scanf("%d", n);
-	scanf("%d", m);
-	int count = 0;
-	int m_temp = m;
-	while (n > 0) {
-		n = n / 10;
-		count += 1;
-	}
-
-	int arr[10] = {0};
-	for (int i = 0; i < count; i++) {
-		arr[i] = m_temp % 10;
-		m_temp = m_temp / 10;
-	}
+int main() {
+    long int n, m;
+    cin >> n;
+    cin >> m;
+    
+    int count = 0;
+    int temp_n = n;
+    while (temp_n > 0) {
+        temp_n /= 10;
+        count += 1;
+    }
+    
+    int x = 1;
+    int sum = 0;
+    int temp;
+    for (int i = 0; i < count; i++) {
+        temp = m % 10;
+        m = m / 10;
+        sum += temp * x;
+        x *= 10;
+    }
+    
+    
+    x = 1;
+    int sum2 = 0;
+    while (m > 0) {
+        temp = m % 10;
+        m = m / 10;
+        sum2 += temp * x;
+        x *= 10;
+    }
+    if (sum < n) {
+        cout << sum2;
+    } else {
+        cout << sum2 + 1;
+    }
+    return 0;
+}
--- B1/ex7.cc
+++ B1/ex7.cc
@@ -1,12 +1,43 @@
-#include<iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-int main() 
+  
+int main()
 {
-	int arr[500000];
-	long int n;
-	scanf("%ld", &n);
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &arr[i]);
-	}
+    int i = 0;
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (i = 0; i < n; i ++) {
+        scanf("%d", &arr[i]);
+    }
+    //sort(arr, arr + n);
+    sort(arr, arr + n, greater<int>());
+    int ans = 0;
+    for (i = 0; i < n; i++) {
+        if (arr[i] > i) {
+            ans++;
+        }
+    }
+    /*
+    int lo = 0;
+    int hi = n - 1;
+    int mid = (lo + hi +1)/2;
+    while (lo <= hi ) {
+        mid = (lo +hi) / 2;
+        if (n - mid <= arr[mid]) {
+            hi = mid - 1;
+            }
+        else {
+            lo = mid +1;
+        }
+    }
+    while (arr[mid - 1] == arr[mid] && arr[mid - 1] >= n - mid + 1) {
+        mid = mid - 1;
+    }
+    
+    int ans = n - mid;
+    */
+    cout << ans;
+    
+    return 0;
+}
